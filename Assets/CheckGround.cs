3790734258d0d36eda08276b3e202bc00416a7d7
Martin Fuentes <mefuentes@uninorte.edu.co>
2019-11-10 18:14:34 -0500

    Detalles de escena, sprites y movimientos

---
+++ Assets/CheckGround.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private PlayerController player;
+    private Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+        rb2D = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Plataform")
+        {
+            player.transform.parent = col.transform;
+            player.grounded = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground") {
+            player.grounded = true;
+        }
+
+        if (col.gameObject.tag == "Plataform")
+        {
+            player.transform.parent = col.transform;
+            player.grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground") {
+        player.grounded = false;
+        }
+
+        if (col.gameObject.tag == "Plataform")
+        {
+            player.transform.parent = null;
+            player.grounded = false;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 2f;
+    public float speed = 15f;
+    public bool grounded;
+    public float jumpForce = 9f;
+    private Rigidbody2D rb2D;
+    private Animator anim;
+    private bool jump; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb2D.velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 fixedVelocity = rb2D.velocity;
+        fixedVelocity.x *= 0.75f;
+
+        if (grounded)
+        {
+            rb2D.velocity = fixedVelocity;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        rb2D.AddForce(Vector2.right * speed * horizontal);
+
+        float limitedSpeed = Mathf.Clamp(rb2D.velocity.x, -maxSpeed, maxSpeed);
+        rb2D.velocity = new Vector2(limitedSpeed, rb2D.velocity.y);
+
+        if (horizontal > 0.1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (horizontal < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (jump)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+
+        Debug.Log(rb2D.velocity.y);
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        transform.position = new Vector3(-2, 0, 0);
+    }
+}
